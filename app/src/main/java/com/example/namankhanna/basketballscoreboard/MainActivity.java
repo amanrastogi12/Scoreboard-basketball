f45510fbbb6744bea7a73e7cc574cfb54f61d6f2
Naman Khanna <namankhanna97@gmail.com>
2018-09-26 17:17:48 +0530

    First commit

--- app/src/main/java/com/example/namankhanna/basketballscoreboard/MainActivity.java
+++ app/src/main/java/com/example/namankhanna/basketballscoreboard/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.namankhanna.basketballscoreboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar myToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myToolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(myToolbar);
+
     }
 }
